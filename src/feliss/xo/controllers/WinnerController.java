cabfcfab98572aec84cdaf90fb0bb8843902e318
feliss <kol.andrey@gmail.com>
2016-05-26 05:25:19 +0300

    fixes WinnerController

--- src/feliss/xo/controllers/WinnerController.java
+++ src/feliss/xo/controllers/WinnerController.java
@@ -37,14 +37,17 @@ public class WinnerController {
         final Point nextPoint = pointGenerator.next(currentPoint);
         try {
             currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(currentPoint);
+
+            if (currentFigure == null)
+                return false;
+
+            nextFigure = field.getFigure(nextPoint);
         } catch (InvalidPointException e) {
             return true;
         }
 
-        if (currentFigure == null) return false;
-
-        if (currentFigure != nextFigure) return false;
+        if (currentFigure != nextFigure)
+            return false;
 
         return check(field, nextPoint, pointGenerator);
     }
